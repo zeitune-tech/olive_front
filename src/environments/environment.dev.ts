382ab6a17366d7dcead1b37d72f49333c3347663
lamine-f <itmouhamedlaminefaye@gmail.com>
2025-07-12 23:18:27 +0000

    Update main.yml

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  auth_url: "http://dev.olive.zeitune.tech:8010/api/v1",
+  auth_url: "https://dev.autholive.zeitune.tech/api/v1",
   administration_url: "http://dev.olive.zeitune.tech:8020/api/v1/app",
   settings_url: "http://dev.olive.zeitune.tech:8030/api/v1/app",
   attestations_url: "http://dev.olive.zeitune.tech:8040/api/v1/app",
