85d937ad099853844937f99968822e91c1931b68
lamine-f <itmouhamedlaminefaye@gmail.com>
2025-07-22 09:17:31 +0000

    setup recipe env

---
+++ src/environments/environment.recipe.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  auth_url: "https://recipe.gatewayolive.zeitune.tech/auth",
+  administration_url: "https://recipe.gatewayolive.zeitune.tech/admin",
+  settings_url: "https://recipe.gatewayolive.zeitune.tech/settings",
+  attestations_url: "https://recipe.gatewayolive.zeitune.tech",
+  insured_url: "https://recipe.gatewayolive.zeitune.tech",
+  production: false
+};
+
