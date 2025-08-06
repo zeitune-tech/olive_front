198aeaba0c8e1f2efcdd85f750df1e61c8062314
lamine-f <itmouhamedlaminefaye@gmail.com>
2025-08-06 08:07:01 +0000

    feat: restructure pricing navigation to include exploitation and sinister sections

--- src/app/core/navigation/nav/pricing.nav.ts
+++ src/app/core/navigation/nav/pricing.nav.ts
@@ -5,93 +5,113 @@ import { PERMISSIONS } from "@core/permissions/permissions.data";
 
 export const defaultPrincingNavigation: NavigationItem[] = [
   {
-    id: 'constants',
-    title: 'sidebar.pricing.constants',
-    type: 'basic',
-    icon: 'fluent:people-team',
-    link: '/pricing/constants',
-    // permission: PERMISSIONS.VIEW_PRICING_CONSTANTS,
-    children: []
-  },
-  {
-    id: 'pricing',
-    title: 'sidebar.pricing.field',
+    id: 'exploitation',
+    title: 'sidebar.pricing.exploitation',
     type: 'collapsable',
     icon: 'fluent:people-team',
-    link: '/pricing/fields',
-    // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+    link: '/pricing/exploitation/constants',
+    // permission: PERMISSIONS.VIEW_PRICING_CONSTANTS,
     children: [
       {
-        id: 'select-field',
-        title: 'sidebar.pricing.select_field',
+        id: 'constants',
+        title: 'sidebar.pricing.constants',
+        type: 'basic',
+        icon: 'fluent:people-team',
+        link: '/pricing/constants',
+        // permission: PERMISSIONS.VIEW_PRICING_CONSTANTS,
+        children: []
+      },
+      {
+        id: 'pricing',
+        title: 'sidebar.pricing.field',
         type: 'collapsable',
         icon: 'fluent:people-team',
-        link: '/pricing/fields/list',
+        link: '/pricing/fields',
         // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
         children: [
           {
-            id: 'select-field-option-value',
-            title: 'sidebar.pricing.select_field_option_value',
-            type: 'basic',
+            id: 'select-field',
+            title: 'sidebar.pricing.select_field',
+            type: 'collapsable',
             icon: 'fluent:people-team',
-            link: '/pricing/fields/select-options/values',
-            // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
+            link: '/pricing/fields/list',
+            // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+            children: [
+              {
+                id: 'select-field-option-value',
+                title: 'sidebar.pricing.select_field_option_value',
+                type: 'basic',
+                icon: 'fluent:people-team',
+                link: '/pricing/fields/select-options/values',
+                // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
+              },
+              {
+                id: 'select-field-option',
+                title: 'sidebar.pricing.select_field_option',
+                type: 'basic',
+                icon: 'fluent:people-team',
+                link: '/pricing/fields/select-options/list',
+                // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
+              },
+
+            ]
           },
           {
-            id: 'select-field-option',
-            title: 'sidebar.pricing.select_field_option',
+            id: 'field',
+            title: 'sidebar.pricing.field_list',
             type: 'basic',
             icon: 'fluent:people-team',
-            link: '/pricing/fields/select-options/list',
-            // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
+            link: '/pricing/fields/list',
+            // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
           },
-
         ]
       },
       {
-        id: 'field',
-        title: 'sidebar.pricing.field_list',
+        id: 'pricing',
+        title: 'sidebar.pricing.variable_condition',
         type: 'basic',
         icon: 'fluent:people-team',
-        link: '/pricing/fields/list',
+        link: '/pricing/variable-conditions/list',
         // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+        children: []
+      },
+      {
+        id: 'pricing',
+        title: 'sidebar.pricing.formula',
+        type: 'collapsable',
+        icon: 'fluent:people-team',
+        link: '/pricing/formula',
+        // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+        children: [
+          {
+            id: 'pricing-formula-create',
+            title: 'sidebar.pricing.formula_create',
+            type: 'basic',
+            icon: 'fluent:people-team',
+            link: '/pricing/formula/create',
+            // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
+          },
+          {
+            id: 'pricing-formula',
+            title: 'sidebar.pricing.formula_list',
+            type: 'basic',
+            icon: 'fluent:people-team',
+            link: '/pricing/formula/list',
+            // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+          },
+        ]
       },
     ]
   },
   {
-    id: 'pricing',
-    title: 'sidebar.pricing.variable_condition',
+    id: 'sinister',
+    title: 'sidebar.pricing.sinister',
     type: 'basic',
     icon: 'fluent:people-team',
-    link: '/pricing/variable-conditions/list',
-    // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
+    link: '/pricing/sinister/constants',
+    // permission: PERMISSIONS.VIEW_PRICING_CONSTANTS,
     children: []
   },
-  {
-    id: 'pricing',
-    title: 'sidebar.pricing.formula',
-    type: 'collapsable',
-    icon: 'fluent:people-team',
-    link: '/pricing/formula',
-    // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
-    children: [
-      {
-        id: 'pricing-formula-create',
-        title: 'sidebar.pricing.formula_create',
-        type: 'basic',
-        icon: 'fluent:people-team',
-        link: '/pricing/formula/create',
-        // permission: PERMISSIONS.CREATE_PRICING_FORMULA,
-      },
-      {
-        id: 'pricing-formula',
-        title: 'sidebar.pricing.formula_list',
-        type: 'basic',
-        icon: 'fluent:people-team',
-        link: '/pricing/formula/list',
-        // permission: PERMISSIONS.VIEW_PRICING_FORMULA,
-      },
-    ]
-  },
+
 ];
 export const compactPrincingNavigation: NavigationItem[] = [];
