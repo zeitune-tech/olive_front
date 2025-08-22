2c2e6c442125cfba5974a69fd0e74e1cc1478e3f
lamine-f <itmouhamedlaminefaye@gmail.com>
2025-08-22 12:16:35 +0000

    feat: add exploitation URL to development environment configuration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,7 @@ export const environment = {
   auth_url: "https://dev.adminolive.zeitune.tech/auth",
   administration_url: "https://dev.adminolive.zeitune.tech/admin/app",
   settings_url: "https://dev.adminolive.zeitune.tech/settings/app",
+  exploitation_url: "https://dev.adminolive.zeitune.tech/exploitation",
   attestations_url: "https://dev.adminolive.zeitune.tech/app",
   insured_url: "https://dev.adminolive.zeitune.tech/app",
   pricing_url: "https://dev.adminolive.zeitune.tech/princing/app",
